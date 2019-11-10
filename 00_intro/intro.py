30429f023b86db1c0400f40556d51f0617e83abb
keshava kotri <keshav143420@gmail.com>
2019-11-10 11:07:40 +0530

    added dynamic property

--- 00_intro/intro.py
+++ 00_intro/intro.py
@@ -2,6 +2,9 @@ from user import User
 
 keshav = User('keshav')
 keshav.say_hello()
+keshav.skills = ['python', 'javascript']
+print(keshav.skills)
+keshav.say_hello()
 
 kumar = User('kumar')
 kumar.say_hello()
